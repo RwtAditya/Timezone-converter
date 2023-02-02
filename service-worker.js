a73cbdb304edac26ed5eec88d917d16c7e7be7ec
RwtAditya <117914221+RwtAditya@users.noreply.github.com>
2023-02-02 21:29:56 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const cacheName = 'v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then((cache) => {
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/style.css',
+          '/script.js',
+        ]);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== cacheName) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
